f7d1689aad2e01231cc33c7e2a68c69b78f11f3d
Alejandro Frías <alejandrofdiaz@gmail.com>
2018-07-20 16:11:45 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const basePath = __dirname;
+
+module.exports = {
+  mode: 'development',
+  resolve: {
+    extensions: ['.js','.ts', '.tsx', '.css']
+  },
+  entry: {
+    app: './src/index.tsx'
+  },
+  output: {
+    path: path.join(basePath, 'dist'),
+    filename: '[chunkhash][name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          },
+          {
+            loader: 'tslint-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: false,
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(scss)$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: false,
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(woff2?|svg|png|jpe?g)$/,
+        loader: 'url-loader?limit=10000'
+      },
+      {
+        test: /\.(ttf|eot)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //Name of file in ./dist/
+      template: './src/index.html', //Name of template in ./src
+      hash: true
+    }),
+    new ExtractTextPlugin({
+      filename: '[chunkhash].[name].css',
+      disable: false,
+      allChunks: true
+    })
+  ]
+};
